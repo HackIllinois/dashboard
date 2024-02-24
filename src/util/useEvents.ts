5668af3f51547cb57a3e63e3790b09b35cec123c
Nick King <nrking0@gmail.com>
2024-02-23 19:50:58 -0600

    Fix events!

--- src/util/useEvents.ts
+++ src/util/useEvents.ts
@@ -11,7 +11,7 @@ export const useEvents = (refreshCycle = 600000) => {
         getEvents().then((res) => setEvents(res.sort((a, b) => a.startTime - b.startTime).filter((e) => e.endTime >= (Date.now()/1000)).slice(0, 3)));
         const intervalId = setInterval(() => {
 
-            getEvents().then((res) => setEvents(res.sort((a, b) => a.startTime - b.startTime).slice(0, 3)));
+            getEvents().then((res) => setEvents(res.sort((a, b) => a.startTime - b.startTime).filter((e) => e.endTime >= (Date.now()/1000)).slice(0, 3)));
             
         }, refreshCycle);
         return () => clearInterval(intervalId);
