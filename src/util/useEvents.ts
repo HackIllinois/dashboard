d16ea2a4d86cf56ab99bb4e1054dfa528982f896
Nick King <nrking0@gmail.com>
2024-02-23 18:14:55 -0600

    Fix events!!!

--- src/util/useEvents.ts
+++ src/util/useEvents.ts
@@ -3,15 +3,14 @@ import { useEffect, useState } from "react";
 import { Event } from "./api";
 
 export const useEvents = (refreshCycle = 600000) => {
-    console.log("useLeaderboard");
     const [events, setEvents] = useState<Event[]>([]);
 
    
 
     useEffect(() => {
-        getEvents().then((res) => setEvents(res.sort((a, b) => a.startTime - b.startTime).slice(0, 3)));
+        getEvents().then((res) => setEvents(res.sort((a, b) => a.startTime - b.startTime).filter((e) => e.endTime >= (Date.now()/1000)).slice(0, 3)));
         const intervalId = setInterval(() => {
-            console.log("fetching leaderboard");
+
             getEvents().then((res) => setEvents(res.sort((a, b) => a.startTime - b.startTime).slice(0, 3)));
             
         }, refreshCycle);
