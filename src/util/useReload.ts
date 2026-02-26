8ec46158d2eec0fe95ebec7207b8a3834a4dea2d
Shreepaad <shreepaad.earanti@gmail.com>
2026-02-26 13:51:43 -0600

    one minute refresh

--- src/util/useReload.ts
+++ src/util/useReload.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 // What interval to reload on
 // not tested outside of 1-5 range - beware
-const EVERY_MINUTES = 5;
+const EVERY_MINUTES = 1;
 
 export default function useTimeSyncedReload() {
   useEffect(() => {
