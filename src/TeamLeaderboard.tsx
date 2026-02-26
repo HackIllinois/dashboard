413b7ca1a8d703037926fb109b5c3b39afc1a791
Shreepaad <shreepaad.earanti@gmail.com>
2026-02-26 14:24:07 -0600

    color changes

--- src/TeamLeaderboard.tsx
+++ src/TeamLeaderboard.tsx
@@ -30,10 +30,11 @@ export default function TeamLeaderboard() {
        marginTop:"1vh",
        border:"0.13vh solid #00FF2B", borderRadius:"3.5vh", 
       background:`
-      linear-gradient(180deg, rgba(0, 135, 3, 0.38), rgba(0, 135, 3, 0.38)),
-      linear-gradient(to right, rgba(84, 172, 72, 0.20) .3vh, transparent 1px),
-      linear-gradient(to bottom, rgba(84, 172, 72, 0.20) .3vh, transparent 1px)
-      `, backgroundSize: "100% 100%, 25% 25%, 25% 25%"}}>
+      linear-gradient(180deg, rgba(1, 98, 2, 0.7), rgba(0, 135, 3, 0.38)),
+      linear-gradient(to right, rgba(32, 122, 20, 0.6) .3vh, transparent 1px),
+      linear-gradient(to bottom, rgba(44, 137, 32, 0.6) .3vh, transparent 1px)
+      `, backdropFilter: "blur(1vh)",
+WebkitBackdropFilter: "blur(1vh)",backgroundSize: "100% 100%, 25% 25%, 25% 25%"}}>
         <img alt="team" src={logoMap[team.name]} style={team.name === "Aquila" ? {width:"15vh", height:"15vh", marginTop:".3vh", marginBottom:".4vh"} : {width:"16vh", height:"16vh"}}></img>
         <div style={{fontSize:"2.2vh", fontFamily:"Tsukimi Rounded", color:"white", fontWeight:700, marginTop:"-1vh"}}>{team.points} Points</div>
         <div style={{fontSize:"2.2vh", fontFamily:"Tsukimi Rounded", color:"white", fontWeight:700}}>{team.members} Members</div>
