3e16bb0c46f123299bf32694033e603d69b42e14
Shreepaad <shreepaad.earanti@gmail.com>
2026-02-26 14:46:45 -0600

    removed blur

--- src/TeamLeaderboard.tsx
+++ src/TeamLeaderboard.tsx
@@ -33,8 +33,10 @@ export default function TeamLeaderboard() {
       linear-gradient(180deg, rgba(1, 98, 2, 0.7), rgba(0, 135, 3, 0.38)),
       linear-gradient(to right, rgba(32, 122, 20, 0.6) .3vh, transparent 1px),
       linear-gradient(to bottom, rgba(44, 137, 32, 0.6) .3vh, transparent 1px)
-      `, backdropFilter: "blur(.2vh)",
-WebkitBackdropFilter: "blur(.2vh)",backgroundSize: "100% 100%, 25% 25%, 25% 25%"}}>
+      `, 
+      // backdropFilter: "blur(.2vh)",
+// WebkitBackdropFilter: "blur(.2vh)",
+backgroundSize: "100% 100%, 25% 25%, 25% 25%"}}>
         <img alt="team" src={logoMap[team.name]} style={team.name === "Aquila" ? {width:"15vh", height:"15vh", marginTop:".3vh", marginBottom:".4vh"} : {width:"16vh", height:"16vh"}}></img>
         <div style={{fontSize:"2.2vh", fontFamily:"Tsukimi Rounded", color:"white", fontWeight:700, marginTop:"-1vh"}}>{team.points} Points</div>
         <div style={{fontSize:"2.2vh", fontFamily:"Tsukimi Rounded", color:"white", fontWeight:700}}>{team.members} Members</div>
